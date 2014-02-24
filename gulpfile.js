3686c1287e53c1e6d32c72a7d606dd675fe00db6
Ezell <iamezell@gmail.com>
2014-02-24 12:29:17 -0500

    this is my commit for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
 gulp.task('default', function(){
   // place code for your default task here
   //testing gulp script 3
+  gulp.src('./lib/*.js')
+    .pipe(concat("all.js"))
+    .pipe(gulp.dest('./dist/'))
 });
