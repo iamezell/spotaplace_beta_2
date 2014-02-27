99fd61c190b378721f0ff0fe7cc2b1d8708e3504
Ezell <iamezell@gmail.com>
2014-02-27 08:22:47 -0500

    commiting changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var inject = require("gulp-inject");
 
 gulp.task('default', function(){
   // place code for your default task here
@@ -7,4 +8,8 @@ gulp.task('default', function(){
   gulp.src('./lib/**/*.js')
     .pipe(concat("all.js"))
     .pipe(gulp.dest('./dist/'))
+
+  gulp.src('./lib/views/.html')
+  .pipe(inject(gulp.src(["./src/*.js", "./src/*.css"], {read: false}))) // Not necessary to read the files (will speed up things), we're only after their paths
+  .pipe(gulp.dest("./dist"));
 });
